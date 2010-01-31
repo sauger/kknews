5a938c0fe8b0c665dfc6ae46ab631b65dc4d0fcc
sauger <sauger@163.com>
2010-01-31 10:33:55 +0800

    adjust index.html

--- javascript/sortable.js
+++ javascript/sortable.js
@@ -19,26 +19,33 @@ $(function(){
 		if($(this).parent().find('input').attr('checked')){
 			$(this).parent().find('input').attr('checked',false);
 			$(this).parent().attr('class','stylecontext1');
+			remove_category($(this).parent().attr('index'));
 		}else{
 			$(this).parent().find('input').attr('checked',true);
 			$(this).parent().attr('class','stylecontext2');
-			add_category($(this).html());
+			add_category($(this).html(),$(this).parent().attr('index'));
 		}
 	});
 	
-	$('.stylecontext1 input').click(function(){
+	
+	$('.stylecontent input:checkbox').click(function(){
 		if($(this).attr('checked')){
-			$(this).attr('checked',false);
-			$(this).parent().attr('class','stylecontext1');
-		}else{
-			$(this).attr('checked',true);
 			$(this).parent().attr('class','stylecontext2');
+			add_category($(this).parent().find('span').html(),$(this).parent().attr('index'));
+			
+		}else{
+			$(this).parent().attr('class','stylecontext1');
+			remove_category($(this).parent().attr('index'));
 		}
 	});
+	
+	function remove_category(id){
+		$('#m_l_m'+ id).remove();
+	}
 	
 	function add_category(caption,id){
-		var str = '<div class=m_l_m id="'+ id + '">'
-				+'<div class=title><div class="title_name">'+caption +'</div><div class=pic2><a href=""><img border=0 class="xf" src="images/xf.gif"></a></div><div class="addjian"><img style="cursor:pointer;" src="images/add.jpg">　<img style="cursor:pointer;" src="images/jian.jpg"></div><div class=more><a href="">更多</a></div></div>'
+		var str = '<div class=m_l_m id="m_l_m'+ id + '">'
+				+'<div class=title><div class="title_name">'+caption +'</div><div class=pic2><a href=""><img border=0 class="xf" src="images/xf.gif"></a></div><div class="addjian"><img style="cursor:pointer;" src="images/add.jpg" class="img_plus">　<img style="cursor:pointer;" src="images/jian.jpg" class="img_mine"></div><div class=more><a href="">更多</a></div></div>'
 				+'<div class=m_context>'
 				+'	<div class=m_content>'
 				+'		<img width="111" height="83" src="images/b1.jpg" id="pop_img">'
@@ -74,5 +81,71 @@ $(function(){
 				+'<div class="m_l_m_b"></div>'
 			+'</div>';
 			$('#sortable').append(str);
+			$('.img_plus').unbind('click');
+			$('.img_mine').unbind('click');
+			bind_plus_mine();
 	};
+	
+	
+	
+	function bind_plus_mine(){
+		$('.img_plus').click(function(){
+			var str = '<div class=m_content>'
+					+'	<img width="111" height="83" src="images/b7.jpg">'
+					+'	<div class=c_title1><a href="">[威尔]</a></div>'
+					+'	<div class=c_content1><a href="">主创莅临蓉城 世界舞王教人跳踢踏</a></div>'
+					+'</div>'
+					+'<div class=m_content>'
+					+'	<img width="111" height="83" src="images/b8.jpg">'
+					+'	<div class=c_title1><a href="">[魏钰婷]</a></div>'
+					+'	<div class=c_content1><a href="">胡歌再次出演电视剧神话</a></div>'
+					+'</div>'
+					+'<div class=m_content>'
+					+'	<img width="111" height="83" src="images/b9.jpg">'
+					+'	<div class=c_title1><a href="">[肖若赝]</a></div>'
+					+'	<div class=c_content1><a href="">黄山有成旅游热门景点</a></div>'
+					+'</div>'
+					+'<div class=m_content>'
+					+'	<img width="111" height="83" src="images/b10.jpg">'
+					+'	<div class=c_title1><a href="">[蔡剑文]</a></div>'
+					+'	<div class=c_content1><a href="">费德勒横扫特松加 小将回球艰难</a></div>'
+					+'</div>'
+					+'<div class=m_content>'
+					+'	<img width="111" height="83" src="images/b11.jpg">'
+					+'	<div class=c_title1><a href="">[张义勇]</a></div>'
+					+'	<div class=c_content1><a href="">小布33分火箭结束三连败</a></div>'
+					+'</div>'
+					+'<div class=m_content>'
+					+'	<img width="111" height="83" src="images/b12.jpg">'
+					+'	<div class=c_title1><a href="">[沐咭咭]</a></div>'
+					+'	<div class=c_content1><a href="">艾弗森23分火力不逊科比</a></div>'
+					+'</div>';
+					//alert($(this).parent().parent().parent().find('.m_context').attr('class'));
+			$(this).parent().parent().parent().find('.m_context').append(str);
+		});
+		$('.img_mine').click(function(){
+			$(this).parent().parent().parent().find('.m_context').find('.m_content:lt(6)').remove();
+		});
+		$('#kb_mine').click(function(){
+			$(this).parent().parent().parent().parent().find('#content').find('.cl:last').remove();
+		});
+		$('#kb_add').click(function(){
+			$(this).parent().parent().parent().parent().find('#content').append('<div class=cl><a href="">·[体育] 《公主和青蛙》迪士尼动画</a></div>');
+		});
+		$('.a_tmp').click(function(e){
+			e.preventDefault();
+		});
+		
+		$('#sc_add').click(function(){
+			$(this).parent().parent().parent().append('<div class=cl><a class="idft3" href="">[博客家园] </a><a class="idft4" href="">三年内可能爆发危机</a></div>');
+		});
+		$('#sc_mine').click(function(){
+			$(this).parent().parent().parent().find('.cl:last').remove();
+		});
+	}	
+	 bind_plus_mine();
+	 $('#btn_save').click(function(){
+	 	$('#div_cfg').hide();
+		$('#img_cfg').attr('src','/images/extract.jpg');
+	 });
 });
